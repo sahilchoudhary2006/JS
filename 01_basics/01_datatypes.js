09368ecf1d117d9356c56cdb4f7e2ebe41fe9000
sahilchoudhary2006 <sahil830261@gmail.com>
2026-06-29 20:20:09 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,29 @@
+"use strict";  //this will treat all JS acode as newer version
+
+// alert(3+3)  // we are using node.js not browser this is for browser 
+
+console.log(3+3); console.log("Sahil") // code readability is not good 
+
+let name = "Sahil"
+
+let age = 20
+
+let isLoggedIn = false
+
+//number => 2 to power 53 this range of number 
+//agar number bahut bada h toh bigint use karte h 
+
+//string => "" double qoutes me likh denge 
+//boolean => true or false
+// null => standalone value and null is also a repesentation for a empty thing
+// undefined => matlab hamen value assign nahi ki hai 
+// symbol => used for unique chizze
+
+
+// object 
+
+console.log(typeof "Sahil")
+
+console.log(typeof null)  // type of null is object 
+
+console.log(typeof undefined) // types of undefined is undifiend
