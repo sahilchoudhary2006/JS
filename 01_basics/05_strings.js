7c5aa7f3b0fc7911957339fd0e137418f5685174
sahilchoudhary2006 <sahil830261@gmail.com>
2026-06-30 13:51:14 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,25 @@
+const name = "sahil"
+const repoCount = 50
+
+console.log(name + repoCount + " value")
+
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`)
+
+const gameName = new String ('sahilsc')
+
+//console.log(gameName.length);
+//console.log(gameName.__proto__);
+
+
+//console.log(gameName.length);
+//console.log(gameName.toUpperCase());
+
+console.log(gameName.charAt(2));
+
+console.log(gameName.indexOf('t'));
+
+const subString = gameName.substring(0, 4)
+console.log(subString);
+
+const anotherString = gameName.slice(1,4)
+console.log(anotherString);
