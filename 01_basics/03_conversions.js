9b3d1accf1cf58152f3b251ff6c30a686304ce59
sahilchoudhary2006 <sahil830261@gmail.com>
2026-06-30 05:12:46 +0000

    conversions and operations

--- 01_basics/03_conversions.js
+++ 01_basics/03_conversions.js
@@ -26,5 +26,30 @@ let somemNumber = 33;
 
 let stringNumber = String(somemNumber);
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+
+// ******************** Operations *************************
+
+let value = 3
+let negValue = -value;
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%3);
+let str1 = "hello"
+let str2 = " Sahil"
+
+let str3 = str1 + str2;
+
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1"+ 2+ 2);
+console.log(1+ 2 + "2");
