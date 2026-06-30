69f679a3fba6c62212d83ed02606e61345c0b85e
sahilchoudhary2006 <sahil830261@gmail.com>
2026-06-30 04:05:09 +0000

    Conversions

---
+++ 01_basics/03_conversions.js
@@ -0,0 +1,30 @@
+let score = "sahil"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber);
+
+
+//"33" => 33
+//"33abc" => NaN
+// true => 1; false => 0;
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// in boolean if we write 1 then it will be converted into true 
+// "" +> empty string me false aata h 
+//"Sahil"=> this will return true 
+
+let somemNumber = 33;
+
+let stringNumber = String(somemNumber);
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
