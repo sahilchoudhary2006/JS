a4796d0a5909beadc3eddc88db44044d5b047cc6
sahilchoudhary2006 <sahil830261@gmail.com>
2026-06-30 13:23:08 +0000

    datatype summary

---
+++ 01_basics/dataTypesSummary.js
@@ -0,0 +1,44 @@
+// primitve 
+
+// 7 types : string , Number , Boolean , null, Undefined , symbol, bigInt 
+
+const score = 100
+const ScoreValue =100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+
+
+
+// Reference type (Non Primitive)
+
+// Array, Objects , Functions   => inka return data type object hota h 
+
+//array
+
+const heros = ["shaktiman", "naagraj", "doga"]
+
+// declartion of object which is being declared by currly braces
+
+let obj = {                         // object jo h jo currly braces me h obj me toh store kiya h 
+    name: "Sahil",
+    age: 20,
+
+}
+
+//declare function we will see more later
+
+const myFunction = function() {
+        console.log("Hello Wolrd")
+}
+
+
+
