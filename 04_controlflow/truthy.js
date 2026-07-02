1f1c379011b17588427fcd19561f608150bde214
sahilchoudhary2006 <sahil830261@gmail.com>
2026-07-02 18:25:47 +0000

    truthy values

---
+++ 04_controlflow/truthy.js
@@ -0,0 +1,41 @@
+const userEmail = []
+
+// if (userEmail) {
+//     console.log("got user email")
+// } else {
+//     console.log("Dont have user email")
+// }
+
+//falsy value
+
+// false, 0, "", -0, BigInt 0n, null , undefined, NaN  baaki inke alawa saare truthy value
+
+// truthy value 
+
+// true, "0", 'false', " ", [], {}, function(){} ye empty function hai 
+
+// if (userEmail.length === 0) {
+//     console.log("Array is empty")
+// }
+
+
+// Nullish coaliescing operator (??): null undefined
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+
+// val1 = undefined ?? 15
+
+val1 = null?? 10?? 15
+
+
+// console.log(val1)
+
+// terniary operator
+
+// condition ? true : false
+
+const IceTEa = 100
+
+IceTEa >=80 ? console.log("less than 80") : console.log("more than 80")
