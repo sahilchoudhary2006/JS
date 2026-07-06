14e72e179f6dc71d88ede2dbce419997099d64f1
sahilchoudhary2006 <sahil830261@gmail.com>
2026-07-06 12:47:09 +0000

    js

---
+++ project3-Digitalclock/chaiaurcode.js
@@ -0,0 +1,14 @@
+document.getElementById('clock')
+// document.querySelector('#clock')
+
+
+
+setInterval(function(){
+
+let date = new Date()
+
+// console.log(date.toLocaleDateString());
+
+CSSLayerBlockRule.innerHTML = date.toLocaleDateString();
+
+}, 1000)
